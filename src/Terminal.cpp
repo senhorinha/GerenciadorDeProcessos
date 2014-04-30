2f9aa5ff54055e944efd2c41f4c937e64f7f9dff
Thiago Rose <thiago.senhorinha@gmail.com>
2014-04-29 23:19:45 -0300

    Melhorando apresentacao do tempo atual

--- src/Terminal.cpp
+++ src/Terminal.cpp
@@ -6,20 +6,23 @@ Terminal::Terminal() {
 
 void Terminal::imprimirProcesso(Processo p) {
 
-	printf("***************************************\n");
-	printf("Nome: %s", p.id.nome.c_str());
-	printf("Usuário: %s", p.id.usuario.c_str());
-	printf("Entrou no sistema em: %lu", p.control.entradaNoSistema);
-	printf("Tempo necessário para execução: %lu",
+	printf("\n***************************************");
+	printf("\nNome: %s", p.id.nome.c_str());
+	printf("\nUsuário: %s", p.id.usuario.c_str());
+	printf("\nEntrou no sistema em: %lu", p.control.entradaNoSistema);
+	printf("\nTempo necessário para execução: %lu",
 			p.control.tempoNecessarioDeCPU);
-	printf("Tempo restante para finalizar execução: %lu",
+	printf("\nTempo restante para finalizar execução: %lu",
 			p.control.tempoNecessarioParaFinalizarExecucao);
-	printf("Número de vezes que já utilizou a CPU: %u",
+	printf("\nNúmero de vezes que já utilizou a CPU: %u",
 			p.control.numeroDeVezesNaCPU);
 }
 
 void Terminal::imprimirTempo(long tempo) {
-	printf("Tempo do Sistema: %lu", tempo);
+	printf("\n");
+	printf("\n###### Relógio ######");
+	printf("\nTempo do Sistema: %lu", tempo);
+	printf("\n### #### #### ### ###");
 }
 
 void Terminal::limparTela() {
